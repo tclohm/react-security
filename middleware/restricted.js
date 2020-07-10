ef22166a9d740d6f6980f05f03fd3384dc885ac6
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-07-10 15:56:17 -0700

    breaking right now due to not setting algo for jwt

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -2,7 +2,7 @@ const jsonwebtoken = require('jsonwebtoken')
 const { secret } = require('../config/secret')
 
 function grabToken(req, res) {
-	res.json({ token: jsonwebtoken.sign({ user: 'johnnyappleseed' }, secret) })
+	return res.json({ token: jsonwebtoken.sign({ user: 'johnnyappleseed' }, secret) })
 }
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require('express-jwt')
+const jsonwebtoken = require('jsonwebtoken')
 const { grabToken, secret } = require('./middleware/restricted')
-
 const app = express()
 
 app.use(cors())
 
-app.get('/jwt', grabToken)
+app.get('/jwt', (req, res) => {
+	res.json({ token: jsonwebtoken.sign({ user: 'john'}, secret )})
+})
 
-// app.use(jwt({ secret: secret }))
+app.use(jwt({ secret: secret }))
 
 const foods = [
 	{ id: 1, item: 'ginger tofu' },
