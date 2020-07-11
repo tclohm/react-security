3f6e36e90dfa229bbe48218c57bd90b50bf9d1a5
Taylor Lohman <taylorlohman@Taylors-MacBook-Pro.local>
2020-07-11 14:47:04 -0700

    setup proxy with cookie

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const cors = require('cors')
 const jwt = require('express-jwt')
-const jsonwebtoken = require('jsonwebtoken')
 const { grabToken, secret } = require('./middleware/restricted')
 const app = express()
 
 app.use(cors())
 
-app.get('/jwt', (req, res) => grabToken(req, res) )
+app.get('/jwt', (req, res) => {
+	const token = grabToken(req, res)
+	// http -- means the cookie cannot be read using JS but can 
+	// still be sent back to the server in HTTP requests
+	// XSS (cross site scripting) attack could use document.cookie
+	// to get a list of stored cookies and their values
+	res.cookie('token', token, { httpOnly: true })
+	res.json({ token })
+})
 
 app.use(jwt({ secret: secret, algorithms: ['HS256'] }))
 
@@ -17,8 +24,6 @@ const foods = [
 	{ id: 3, item: 'caramelized shallot pasta' }
 ]
 
-app.get('/foods', (req, res) => {
-	res.json(foods)
-})
+app.get('/foods', (req, res) => res.json(foods))
 
 module.exports = app;
